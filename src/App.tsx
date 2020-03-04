e711525a9698d3028e4992eded448d741f6a9c48
Fernando Marinho da Silva <marinhosilva.fernando@gmail.com>
2020-03-04 17:20:41 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello from aws.
         </p>
         <a
           className="App-link"
