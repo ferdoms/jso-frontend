bbc202ab2851e34bdfc7f533d84da5979c35e74f
Fernando <marinhosilva.fernando@gmail.com>
2020-03-16 11:13:16 +0000

    search filtering implemented

--- src/components/job-appl-board/JobApplBoard.tsx
+++ src/components/job-appl-board/JobApplBoard.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import Tab from "../tab/Tab";
 import ListJobAppl from "../list/ListJobAppl";
 import { jobApplList } from "../../pages/dashboard-page/mockData";
+import { Input } from "../input/Input";
 
 interface Props {
-
-  jobApplList: any []
+  jobApplList: any[];
 }
 
 interface State {
   activeTab: string;
+  searchFilter: string;
 }
 
 class JobApplBoard extends React.Component<Props, State> {
@@ -17,30 +18,59 @@ class JobApplBoard extends React.Component<Props, State> {
     super(props);
     this.state = {
       activeTab: "Active",
+      searchFilter: ""
     };
-    this._handleTabChange = this._handleTabChange.bind(this)
+    this._handleTabChange = this._handleTabChange.bind(this);
+    this._handleSearchChange = this._handleSearchChange.bind(this);
   }
 
-  private _handleTabChange(activeTab:string){
-    this.setState({activeTab})
+  private _handleTabChange(activeTab: string) {
+    this.setState({ activeTab });
+  }
+  private _handleSearchChange(e: any) {
+    let state: any = this.state;
+    state[e.target.id] = e.target.value;
+    this.setState(state);
   }
   render() {
     const { jobApplList } = this.props;
-    const { activeTab } = this.state;
+    const { activeTab, searchFilter } = this.state;
 
-    let jobsFiltered = this.props.jobApplList.filter((job:any)=>job.status === this.state.activeTab)
-
-
-    return <div>
-      <Tab onChange={this._handleTabChange}/>
-
-      <div className="flex flex-wrap">
-        <ListJobAppl jobApplList={activeTab==="All" ? jobApplList : jobsFiltered}/>
-      </div>
-      
-      
+    let jobsFiltered;
     
-    </div>;
+    if(activeTab === "All") {
+      jobsFiltered = jobApplList
+    }else {
+      jobsFiltered = this.props.jobApplList.filter(
+        (job: any) => job.status === activeTab 
+      );
+    } 
+
+    if (searchFilter !== "")
+      jobsFiltered = jobsFiltered.filter(
+        (job: any) =>
+          job.companyName.includes(searchFilter) ||
+          job.jobDescription.includes(searchFilter) ||
+          job.jobTitle.includes(searchFilter)
+      );
+
+    return (
+      <div>
+        <Tab onChange={this._handleTabChange} />
+        <Input
+          id="searchFilter"
+          inputLabel="Search"
+          onChange={this._handleSearchChange}
+          type="string"
+          value={searchFilter}
+        />
+        <div className="flex flex-wrap">
+          <ListJobAppl
+            jobApplList={jobsFiltered}
+          />
+        </div>
+      </div>
+    );
   }
 }
 export default JobApplBoard;
